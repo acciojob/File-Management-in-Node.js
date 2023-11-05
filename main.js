ded86ee39d7947b57302b9084040f8b853d30ac1
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 14:07:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
 const fs = require('fs');
-const { promisify } = require('util');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const path = require('path');
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
-
-/**
- * Reads multiple CSV files and merges them into a single file.
- * @param {string[]} files - An array of file paths to read.
- * @param {string} outputFile - The file path to write the merged data to.
- * @returns {Promise<void>}
- */
-async function mergeCsvFiles(files, outputFile) {
-  // TODO: Implement the mergeCsvFiles function
+function extractFunctions(directoryPath) {
+  // TODO: Implement this function
 }
+
+module.exports = extractFunctions;
